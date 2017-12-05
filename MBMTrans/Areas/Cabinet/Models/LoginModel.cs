cb6811ed4986ba45df9aab2d917169846afc8321
chii chobits <velikiinyashko@live.ru>
2017-12-05 15:42:54 +0700

    update

--- MBMTrans/Areas/Cabinet/Models/LoginModel.cs
+++ MBMTrans/Areas/Cabinet/Models/LoginModel.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace MBMTrans.Areas.Cabinet.Models
 {
     public class LoginModel
     {
+        public int Id { get; set; }
+        [Required]
         public string Login { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
